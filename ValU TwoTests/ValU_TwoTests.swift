c8603382ba686053deedeb7cde4b8431b33182a5
Clayton Wilson <clay@wilsonfoo.com>
2019-06-05 19:29:06 -0500

    initial commit

---
+++ ValU TwoTests/ValU_TwoTests.swift
@@ -0,0 +1,34 @@
+//
+//  ValU_TwoTests.swift
+//  ValU TwoTests
+//
+//  Created by Clayton Wilson on 6/5/19.
+//  Copyright © 2019 Clayton Wilson. All rights reserved.
+//
+
+import XCTest
+@testable import ValU_Two
+
+class ValU_TwoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
